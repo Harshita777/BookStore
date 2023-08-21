595a75c2a05358b23a869417e6fd98012d95038a
harshitakumari4623 <harshitakumari4623@gmail.com>
2023-08-21 19:52:55 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,26 +1,21 @@
-import React from 'react'
-import  ReactDOM  from 'react-dom/client'
-import './index.css'
-import { books } from './Books'
-import Book from './Book'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { books } from "./Books";
+import Book from "./Book";
 
-function Booklist() {
-    
-    return(
-        <>
-        <h1 className="heading">amazon best sellers</h1>
-      <div className='book-list'>
-        
-        {books.map((book,index) => {
-          
-          return <Book {...book} key={book.id} number = {index}/>
+function BookList() {
+  return (
+    <>
+      <h1 className="heading">amazon best sellers</h1>
+      <div className="book-list">
+        {books.map((book, index) => {
+          return <Book {...book} key={book.id} number={index} />;
         })}
       </div>
-      </>
-        
-    )
-  }
+    </>
+  );
+}
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<Booklist/>)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BookList />);
